4a0316f9bfb78e69a7576fba7720bc73446f66f0
lcontr1 <contrerasliliana22@gmail.com>
2023-12-12 11:41:38 -0600

    contact table added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import ContactList from './components/ContactList'
 
 function App() {
   
@@ -7,7 +8,7 @@ function App() {
   return (
    
       <div>
-        
+        <ContactList />
       </div>
       
   )
